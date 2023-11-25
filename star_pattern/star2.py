2f94149bfd53169c06af03f31262a8d49df330ba
toushifA <toushif1611@gamil.com>
2023-11-25 10:47:10 +0530

    Add Initial Commit

--- star_pattern/star2.py
+++ star_pattern/star2.py
@@ -3,15 +3,18 @@
 
 import turtle
 
+#set screen
 s = turtle.Screen()
 s.bgcolor("black")
 s.tracer(0)
 
+#pen
 a = turtle.Turtle()
 a.color("white")
 a.speed(0)
 a.hideturtle()
 
+#main turtle loop
 while True:
     s.update()
     for i in range(120):
--- star_pattern/star9.py
+++ star_pattern/star9.py
@@ -2,15 +2,19 @@
 # by toushifA_1611
 
 import turtle
+
+#set screen
 s = turtle.Screen()
 s.bgcolor("black")
 s.tracer(0)
 
+#pen
 a = turtle.Turtle()
 a.color("white")
 a.speed(0)
 a.hideturtle()
 
+#main turtle loop
 while True:
     s.update()
     for i in range(200):
