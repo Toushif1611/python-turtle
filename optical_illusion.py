69c89692c19511db6ad6f208cf0b0b83a4ac6914
Toushif1611 <toushif1611@gmail.com>
2026-02-26 15:51:13 +0530

    Add Initial Files

---
+++ optical_illusion.py
@@ -0,0 +1,22 @@
+import turtle
+
+# set screen
+screen = turtle.Screen()
+screen.bgcolor("black")
+screen.tracer(0)
+
+# set pen
+pen =turtle.Turtle()
+pen.hideturtle()
+pen .speed(0)
+
+colors=["red","purple","yellow","green","pink","orange"]
+
+# main function
+for i in range(300):
+    pen.pencolor(colors[i%4])
+    pen.width(i//100+1)
+    pen.forward(i)
+    pen.left(59)
+
+screen.mainloop()
